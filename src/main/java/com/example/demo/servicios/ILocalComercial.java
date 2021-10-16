2b6f5c60b72138caf5a56600eb9291402896e631
Lupevalencia <g.valencia@preving.com>
2021-10-16 22:39:03 +0200

    nuevo DEMO

---
+++ src/main/java/com/example/demo/servicios/ILocalComercial.java
@@ -0,0 +1,54 @@
+
+package com.example.demo.servicios;
+
+import com.example.demo.modelo.Producto;
+import com.example.demo.modelo.Venta;
+
+public interface ILocalComercial {
+    // NOS OLVIDAMOS DE LOS FICHEROS y DEBEMOS DE CREAR LOS MÉTODOS QUE NECESITEMOS MANDAR A LLAMAR PARA VALIDAR
+    //SÓLO NECESITAREMOS PASARLE POR PARÁMETROS LOS OBJETOS A INSERTAR Y LOS DATOS CONCRETOS A BUSCAR
+    
+
+    /**
+     * Insertar los datos del producto que deseamos agregar al catálogo de Productos
+     * @param productoAAgregar 
+     */
+    void ingresarProductos(Producto productoAAgregar);
+    
+    /**
+     * Insertar los datos de la venta que se va a realizar al catálogo de Ventas
+     * @param ventaAAgregar 
+     */
+    void ingresarVentas(Venta ventaAAgregar);
+    
+    /**
+     * El método nos devuelve el número del vendedor con mayor cantidad de ventas realizadas
+     * @return 
+     */
+    int vendedorConMayorCantidadVentas();
+    
+    /**
+     * Listar las ventas ordenadas por número de vendedor de todas aquellas que se han realizado por débito. Además,
+     * hay que incluir el precio final de la venta = precioUnitario* cantidad
+     */
+    void listarOrdenadoVentasDebito(); 
+    
+    /**
+     * Determinar si el vendedor X realizó la venta del producto Y. En tal caso,
+     * mostrar dicha información junto con el precio total de la venta. Si por el contrario no existe, informar sobre ello
+     * @param venta
+     * @return 
+     */
+    String comprobarVentaRealizadaPorVendedor(Venta venta); //Le pasamos la venta porque se trata de una en concreto
+    
+    /**
+     * Calcular el monto total de las ventas realizadas
+     * @return 
+     */
+    float montoTotalVentas();
+    
+    /**
+     * Mostrar los datos de la mayor venta abonada con tarjetade crédito
+     */
+    void DatosVentaMayorTarjetaCredito();
+}
---
+++ src/main/java/com/example/demo/servicios/LocalComercialImpl.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.servicios;
+
+import com.example.demo.modelo.Producto;
+import com.example.demo.modelo.Venta;
+
+/**
+ *
+ * @author g.valencia_preving
+ */
+public class LocalComercialImpl implements ILocalComercial{
+
+    @Override
+    public void ingresarProductos(Producto productoAAgregar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ingresarVentas(Venta ventaAAgregar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int vendedorConMayorCantidadVentas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void listarOrdenadoVentasDebito() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String comprobarVentaRealizadaPorVendedor(Venta venta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float montoTotalVentas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void DatosVentaMayorTarjetaCredito() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
